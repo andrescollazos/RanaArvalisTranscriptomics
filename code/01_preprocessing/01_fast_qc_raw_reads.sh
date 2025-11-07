985d9091fb32bc4ba59429c66c3dc43c0c951334
Felipe Collazos <afcollazosr@gmail.com>
2025-11-07 13:25:21 +0100

    FastQC on the raw reads

---
+++ code/01_preprocessing/01_fast_qc_raw_reads.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH -A uppmax2025-2-221
+#SBATCH -M rackham
+#SBATCH -J FastQC
+#SBATCH -e %x.er
+#SBATCH -t 5:00:00
+#SBATCH -n 2
+#SBATCH --mail-user andres-felipe.collazos-rozo.6881@student.uu.se
+#SBATCH --mail-type=ALL
+
+module load bioinfo-tools
+module load FastQC/0.11.9
+
+source ../../.env
+OUT="$DIR/results/01_preprocessing/fastqc_raw"
+READS="$DIR/data/raw_reads/P32262_*/*.fastq.gz"
+
+fastqc $READS -o $OUT -t 8
