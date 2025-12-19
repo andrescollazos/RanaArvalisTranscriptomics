66292eeda09168c6eb26921f32b69d8828371c9f
Felipe Collazos <afcollazosr@gmail.com>
2025-12-19 12:08:08 +0100

    Produce correlation matrices grouping by baltic side

---
+++ code/05_quality_check/06_correlation_matrices.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -l
+#SBATCH -A uppmax2025-2-221
+#SBATCH -M rackham
+#SBATCH -J PtRQCBaltic
+#SBATCH -e %x.%A_%a.er
+#SBATCH -o %x.%A_%a.out
+#SBATCH -t 4:00:00
+#SBATCH -p node
+#SBATCH --array=0-7
+#SBATCH --cpus-per-task=20
+#SBATCH --mail-user=andres-felipe.collazos-rozo.6881@student.uu.se
+#SBATCH --mail-type=ALL
+
+module load bioinfo-tools
+source ../../.env
+
+dir="$DIR/results/05_quality_check/multi_qc_251215"
+cd $dir
+CONFIG="04_multi_qc.config.tsv"
+read analysis samples_type min_rowSums top_genes top_variable_genes var_gene_method < <(sed -n "$((SLURM_ARRAY_TASK_ID+2))p" $CONFIG)
+
+samples_baltic="$dir/baltic.samples.txt"
+cd $analysis
+
+echo "Sample correlation matrix"
+singularity exec --cleanenv \
+	--env LC_ALL=C \
+	$TRINITY_SINGULARITY/trinityrnaseq.v2.15.2.simg \
+	/usr/local/bin/Analysis/DifferentialExpression/PtR --matrix $matrix \
+		--samples $samples_baltic \
+		--log2 --CPM \
+		--min_rowSums $min_rowSums \
+		--top_genes $top_genes \
+		--top_variable_genes $top_variable_genes \
+		--var_gene_method $var_gene_method \
+		--sample_cor_matrix \
+		--output "${analysis}.baltic.cor_matrix"
