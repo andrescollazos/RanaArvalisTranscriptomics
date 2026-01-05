f74cafc3bd1306b7a8f2dc740416711f64db1097
Felipe Collazos <afcollazosr@gmail.com>
2026-01-05 19:49:05 +0100

    Run Trinotate

---
+++ code/07_annotation/04_03_run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -l
+#SBATCH -A uppmax2025-2-482
+#SBATCH -M pelle
+#SBATCH -J TrinotateRun
+#SBATCH -e %x.%j.er
+#SBATCH -o %x.%j.out
+#SBATCH -t 16:00:00
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=20
+#SBATCH --mail-type=ALL
+
+source ../../.env
+
+singularity exec \
+	--bind $DIR:$DIR:rw \
+	--cleanenv \
+	--env LC_ALL=C \
+	$TRINOTATE_SINGULARITY/trinotate.v4.0.2.simg \
+	/usr/local/src/Trinotate/Trinotate \
+	--db RanaArvalis_Trinotate.sqlite \
+	--CPU $SLURM_CPUS_PER_TASK \
+	--transcript_fasta $DIR/results/07_annotation/Trinity.fasta \
+	--transdecoder_pep $DIR/results/07_annotation/Trinity.fasta.TD2.pep \
+	--trinotate_data_dir $DIR/results/07_annotation/trinotate_data \
+	--run "swissprot_blastp swissprot_blastx pfam infernal" \
+	--use_diamond
